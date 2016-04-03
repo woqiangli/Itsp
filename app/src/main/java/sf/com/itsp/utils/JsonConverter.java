d5e221afd4e4ee9a8d19ce8f150d07465adc621b
zy <zy@zys-iMac.local>
2016-04-03 10:49:09 +0800

    [zy] generate converter to support json

--- app/src/main/java/sf/com/itsp/utils/JsonConverter.java
+++ app/src/main/java/sf/com/itsp/utils/JsonConverter.java
@@ -1,9 +1,46 @@
 package sf.com.itsp.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static sf.com.itsp.utils.StringUtil.isBlank;
+
 public class JsonConverter {
 
     public static <T> String toJson(T t) {
-        return "";
+        return getGson().toJson(t);
     }
 
+    public static <T> T jsonToObject(String json, TypeToken typeToken) {
+        return getGson().fromJson(json, typeToken.getType());
+    }
+
+    private static Gson getGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        return gsonBuilder.create();
+    }
+
+    public static <T> List<T> jsonFromObjectList(String json, TypeToken typeToken) {
+        List<T> list = new ArrayList<T>();
+
+        if (isBlank(json)) {
+            return list;
+        }
+
+        try {
+            T[] o = getGson().fromJson(json, typeToken.getType());
+            list = asList(o);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
--- app/src/test/java/sf/com/itsp/utils/JsonConverterTest.java
+++ app/src/test/java/sf/com/itsp/utils/JsonConverterTest.java
@@ -1,6 +1,10 @@
 package sf.com.itsp.utils;
 
+import com.google.gson.reflect.TypeToken;
+
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -18,7 +22,36 @@ public class JsonConverterTest {
         String json = JsonConverter.toJson(jsonObject);
 
         // then
-        assertThat(json).isEqualTo("");
+        assertThat(json).isEqualTo("{\"string\":\"test\",\"integer\":1,\"anyBoolean\":false}");
+    }
+
+    @Test
+    public void should_parse_single_object_from_json() {
+        // given
+        String json = "{\"string\":\"test\",\"integer\":1,\"anyBoolean\":false}";
+
+        // when
+        JsonObject jsonToObject = JsonConverter.jsonToObject(json, TypeToken.get(JsonObject.class));
+
+        // then
+        assertThat(jsonToObject.string).isEqualTo("test");
+        assertThat(jsonToObject.integer).isEqualTo(1);
+        assertThat(jsonToObject.anyBoolean).isFalse();
+    }
+
+    @Test
+    public void should_parse_list_from_json() {
+        // given
+        String json = "[{\"string\":\"test\",\"integer\":1,\"anyBoolean\":false},{\"string\":\"test2\",\"integer\":2,\"anyBoolean\":false}]";
+
+        // when
+        List<JsonObject> jsonObjects = JsonConverter.jsonFromObjectList(json, TypeToken.get(JsonObject[].class));
+
+        // then
+        assertThat(jsonObjects).hasSize(2);
+        assertThat(jsonObjects.get(1).string).isEqualTo("test2");
+        assertThat(jsonObjects.get(1).integer).isEqualTo(2);
+        assertThat(jsonObjects.get(1).anyBoolean).isFalse();
     }
 
     private class JsonObject {
