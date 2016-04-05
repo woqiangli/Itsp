722c2a94f881d390e2e792865f607c729e83d4be
Wu Xia <wuxia7@sf-express.com>
2016-04-05 17:28:11 +0800

    remove the testing code

--- app/src/main/java/sf/com/itsp/MainActivity.java
+++ app/src/main/java/sf/com/itsp/MainActivity.java
@@ -33,9 +33,6 @@ public class MainActivity extends Activity {
     private void initListView() {
         ListView listView = (ListView) findViewById(R.id.order_list);
         CarrierOrderAdapter adapter = new CarrierOrderAdapter(getApplicationContext());
-        Order order = new Order();
-        order.setVehicleType("货柜车");
-        orderList.add(order);
         adapter.setOrderList(orderList);
         listView.setAdapter(adapter);
     }
