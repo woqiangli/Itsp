18816e5ee55b3fcab3d53cec8c364341aa03200a
zy-3 <zhang>
2016-04-20 10:21:44 +0800

    [zy] -fix code about faking data for http request

--- app/src/test/java/sf/com/itsp/shadows/ShadowConnectionProxy.java
+++ app/src/test/java/sf/com/itsp/shadows/ShadowConnectionProxy.java
@@ -4,16 +4,27 @@ import android.content.Context;
 
 import org.robolectric.annotation.Implements;
 
-@Implements(sf.com.itsp.utils.ConnectionProxy.class)
+import java.util.List;
+
+import sf.com.itsp.domain.Order;
+import sf.com.itsp.utils.ConnectionProxy;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+@Implements(ConnectionProxy.class)
 public class ShadowConnectionProxy {
 
-    public static String orders;
+    private static List<Order> orders = newArrayList();
 
-    public String requestOrder(Context context) {
+    public List<Order> requestOrder(Context context) {
         return orders;
     }
 
+    public static void fakeOrders(List<Order> fakedOrders) {
+        orders = fakedOrders;
+    }
+
     public static void clearOrders() {
-        orders = "[]";
+        orders = newArrayList();
     }
 }
--- app/src/test/java/sf/com/itsp/utils/OrderProvider.java
+++ app/src/test/java/sf/com/itsp/utils/OrderProvider.java
@@ -1,32 +1,39 @@
 package sf.com.itsp.utils;
 
+import com.google.gson.reflect.TypeToken;
+
+import sf.com.itsp.domain.Order;
 import sf.com.itsp.shadows.ShadowConnectionProxy;
 
+import static sf.com.itsp.shadows.ShadowConnectionProxy.fakeOrders;
+
 public class OrderProvider {
+    public static final String ORDERS = "[\n" +
+            "    {\n" +
+            "        'original': 'original1',\n" +
+            "        'target': 'target1',\n" +
+            "        'weight': 2.0,\n" +
+            "        'vehicleType': '车型'," +
+            "        'vehicleAge': 1," +
+            "        'startDate': '2016-04-03'," +
+            "        'endDate': '2016-04-05'" +
+            "    },\n" +
+            "    {\n" +
+            "        'original': 'original2',\n" +
+            "        'target': 'target2',\n" +
+            "        'weight': 2.0,\n" +
+            "        'vehicleType': '车型2'," +
+            "        'vehicleAge': 2," +
+            "        'startDate': '2016-04-03'," +
+            "        'endDate': '2016-04-06'" +
+            "    }\n" +
+            "]";
+
     public static void mockEmptyOrderResponse() {
         ShadowConnectionProxy.clearOrders();
     }
 
     public static void mockOrderResponse() {
-        ShadowConnectionProxy.orders = "[\n" +
-                "    {\n" +
-                "        'original': 'original1',\n" +
-                "        'target': 'target1',\n" +
-                "        'weight': 2.0,\n" +
-                "        'vehicleType': '车型'," +
-                "        'vehicleAge': 1," +
-                "        'startDate': '2016-04-03'," +
-                "        'endDate': '2016-04-05'" +
-                "    },\n" +
-                "    {\n" +
-                "        'original': 'original2',\n" +
-                "        'target': 'target2',\n" +
-                "        'weight': 2.0,\n" +
-                "        'vehicleType': '车型2'," +
-                "        'vehicleAge': 2," +
-                "        'startDate': '2016-04-03'," +
-                "        'endDate': '2016-04-06'" +
-                "    }\n" +
-                "]";
+        fakeOrders(JsonConverter.<Order>jsonFromObjectList(ORDERS, TypeToken.get(Order[].class)));
     }
 }
