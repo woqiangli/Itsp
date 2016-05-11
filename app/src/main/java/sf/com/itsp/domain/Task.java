8e60f33da7166b43945e09cba3315c4287c16693
郑萍玲(Pingling Zheng)-大营运研发中心 <330356@sf.com>
2016-05-11 16:58:20 +0800

    [zpl and wx]add line address information in task

---
+++ app/src/main/java/sf/com/itsp/domain/Task.java
@@ -0,0 +1,18 @@
+package sf.com.itsp.domain;
+
+public class Task {
+
+    private String address;
+
+    public Task(String address) {
+        this.address = address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
