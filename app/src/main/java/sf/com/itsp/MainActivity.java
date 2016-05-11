8994ea1ad634865c4ae62f2e9165efd09d512da0
郑萍玲(Pingling Zheng)-大营运研发中心 <330356@sf.com>
2016-05-11 16:58:20 +0800

    [zpl and wx]add line address information in task

--- app/src/main/java/sf/com/itsp/MainActivity.java
+++ app/src/main/java/sf/com/itsp/MainActivity.java
@@ -2,6 +2,14 @@ package sf.com.itsp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sf.com.itsp.domain.Task;
+import sf.com.itsp.tasks.TaskAdapter;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +17,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Task> tasks = new ArrayList<Task>();
+        tasks.add(new Task("深圳市中转场"));
+        tasks.add(new Task("长沙中转场"));
+        TaskAdapter adapter = new TaskAdapter(MainActivity.this,R.layout.task_item,tasks);
+        ListView listView = (ListView) findViewById(R.id.task_list);
+        listView.setAdapter(adapter);
     }
 
 }
